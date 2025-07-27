305bc4d3410900538fe0ca6e71cf5f16a8ce5467
spiriiito <imitation.2001@mail.ru>
2025-07-27 19:18:15 +0200

    Secured MongoDB credentials, updated db.js and .env.example

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = () => {
-    mongoose.connect('mongodb+srv://spiriiito:UDs4NEXXylXD4whS@cluster0.fyzxe.mongodb.net/project_wp', )
+    mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("DB connected"))
-}
+    .catch((err) => {
+        console.error("MongoDB connection error: ", err);
+        process.exit(1);
+    });
+};
 
