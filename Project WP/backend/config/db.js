098c5090399b7dbadf6dcf495e7c4fee434e6a86
spiriiito <imitation.2001@mail.ru>
2025-07-29 17:26:42 +0200

    Cleaned structure, secured db.js and .env.example

--- Project WP/backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = () => {
-    mongoose.connect('mongodb+srv://spiriiito:UDs4NEXXylXD4whS@cluster0.fyzxe.mongodb.net/project_wp', )
-    .then(() => console.log("DB connected"))
-}
-
